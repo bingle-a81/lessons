8651206f609d5b0ce9186bf1af432072235b2769
notebook <email@example.com>
2025-09-20 11:27:53 +0300

    1

--- 1.py
+++ 1.py
@@ -1,1 +1,12 @@
 print('hello')
+if 1:
+    if 2:
+        print('hello')
+    else:
+        print('world')
+
+for i in range(10):
+    if i % 2 == 0:
+        print(i)
+    else:
+        print(i)
