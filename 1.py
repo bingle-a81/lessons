8eb2a7a61abb9772b08cf3d46405573effb62b4f
notebook <email@example.com>
2025-09-24 22:18:09 +0300

    1

--- 1.py
+++ 1.py
@@ -1,12 +1,42 @@
-print('hello')
-if 1:
-    if 2:
-        print('hello')
-    else:
-        print('world')
+import threading
+import time
+import queue
 
-for i in range(10):
-    if i % 2 == 0:
-        print(i)
-    else:
-        print(i)
+def source():
+    for i in range(20):
+        time.sleep(i/10)
+        yield i
+
+def worker(q):
+    time.sleep(q/2)
+    print(f'эл {q} обработан {threading.get_ident()} ')
+
+my_queue = queue.Queue()
+
+def producer():
+    for element in source():
+        my_queue.put(element)
+        a= threading.get_ident()
+        print(f'эл {element} был доб {a} в очередь {my_queue.qsize()}')
+    my_queue.put(None)
+    print(f'очередь заполнена')
+
+def consumer():
+    while True:
+        elem=my_queue.get()
+        if elem is None:
+            my_queue.put(None)
+            print(f'клиент закончил работу {threading.get_ident()}')
+            break
+        worker(elem)
+
+thred_producer = threading.Thread(target=producer, daemon=True)
+thred_producer.start()
+
+thread_consumer1 = threading.Thread(target=consumer, daemon=True)
+thread_consumer2 = threading.Thread(target=consumer, daemon=True)
+
+thread_consumer1.start()
+thread_consumer2.start()
+thread_consumer1.join()
+thread_consumer2.join()
